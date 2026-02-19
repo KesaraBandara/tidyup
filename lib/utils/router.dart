b6ed125d6237a619b5ce6e80ec6da3cdb80a3aa4
Harsha <kesrabandara1998@gmail.com>
2026-02-19 08:03:16 +0530

    set homepage(main) rout

--- lib/utils/router.dart
+++ lib/utils/router.dart
@@ -1,6 +1,18 @@
+import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
+import 'package:tidyup/pages/home_page.dart';
 
-class Approuter{
-
-  static final router = GoRouter(routes: routes)
+class Approuter {
+  static final router = GoRouter(
+    navigatorKey: GlobalKey<NavigatorState>(),
+    debugLogDiagnostics: true,
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) {
+          return const HomePage();
+        },
+      ),
+    ],
+  );
 }
