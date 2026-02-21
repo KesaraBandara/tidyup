28b76038af6a2bc7631b773c93d70a4233f2e1fd
Harsha <kesrabandara1998@gmail.com>
2026-02-21 08:12:58 +0530

    added note card and todo card to home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tidyup/utils/constants.dart';
 import 'package:tidyup/utils/text_styles.dart';
+import 'package:tidyup/widget/notes_todo_card.dart';
 import 'package:tidyup/widget/progress_card.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,18 +15,32 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Tidyup", 
-        style: AppTextStyles.appTitle)),
-        body: Padding(padding: const EdgeInsets.all(8),
+      appBar: AppBar(title: Text("Tidyup", style: AppTextStyles.appTitle)),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
         child: Column(
           children: [
             SizedBox(height: AppConstants.kDefaultPadding),
-            ProgressCard(
-              completedTasks: 5, 
-              totalTasks: 10,
+            ProgressCard(completedTasks: 5, totalTasks: 10),
+            SizedBox(height: AppConstants.kDefaultPadding * 1.5),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                NotesTodoCard(
+                  title: "Notes",
+                  description: "",
+                  icon: Icons.bookmark_add_outlined,
+                ),
+                 NotesTodoCard(
+                  title: "TO-DO List",
+                  description: "",
+                  icon: Icons.today_outlined,
+                ),
+              ],
             ),
           ],
-        ),),);
+        ),
+      ),
+    );
   }
 }
