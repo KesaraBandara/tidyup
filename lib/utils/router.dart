e83e5ccd3f86eadad271eb17251af4573008e48c
Harsha <kesrabandara1998@gmail.com>
2026-02-23 10:47:28 +0530

    create routers(path) to note and todo pages in to router file

--- lib/utils/router.dart
+++ lib/utils/router.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tidyup/pages/home_page.dart';
+import 'package:tidyup/pages/notes_page.dart';
+import 'package:tidyup/pages/todo_page.dart';
 
-class Approuter {
+class AppRouter {
   static final router = GoRouter(
     navigatorKey: GlobalKey<NavigatorState>(),
     debugLogDiagnostics: true,
@@ -13,6 +15,20 @@ class Approuter {
         path: '/',
         builder: (context, state) {
           return const HomePage();
+        },
+      ),
+      GoRoute(
+        name: "notes",
+        path: '/notes',
+        builder: (context, state) {
+          return const NotesPage();
+        },
+      ),
+      GoRoute(
+        name: "todos",
+        path: '/todos',
+        builder: (context, state) {
+          return const TodoPage();
         },
       ),
     ],
