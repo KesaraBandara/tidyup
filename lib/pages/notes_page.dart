74ca3cf56e1e04c876369a8043d2ce229c163dc1
Harsha <kesrabandara1998@gmail.com>
2026-02-24 01:28:26 +0530

    create methord to store the data in the local storage using hive

--- lib/pages/notes_page.dart
+++ lib/pages/notes_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tidyup/models/note_model.dart';
+import 'package:tidyup/services/note_service.dart';
 import 'package:tidyup/utils/constants.dart';
 import 'package:tidyup/utils/router.dart';
 import 'package:tidyup/utils/colors.dart';
@@ -12,6 +14,40 @@ class NotesPage extends StatefulWidget {
 }
 
 class _NotesPageState extends State<NotesPage> {
+  final NoteService noteService = NoteService();
+  List<Note> allNotes = [];
+  Map<String, List<Note>> noteWithCategory = {};
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    checkAndCreateData();
+  }
+
+  //check if user is new
+  void checkAndCreateData() async {
+    final bool isNewUser = await noteService.isNewUser();
+    //if the user is new create the intial notes
+    if (isNewUser) {
+      await noteService.createInitialNotes();
+    }
+
+    //load the notes
+    _loadNotes();
+  }
+
+  //load the notes
+  Future<void> _loadNotes() async {
+    final List<Note> loadedNotes = await noteService.loadNotes();
+    final Map<String, List<Note>> notesByCategory = noteService
+        .getNotesByCategoryMap(loadedNotes);
+    setState(() {
+      allNotes = loadedNotes;
+      noteWithCategory = notesByCategory;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,17 +69,8 @@ class _NotesPageState extends State<NotesPage> {
         child: Icon(Icons.add, color: AppColors.kWhiteColor, size: 30),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(
-          AppConstants.kDefaultPadding
-          ),
-          child: Column(
-            children: [
-              Text(
-                "Notes",
-                style: AppTextStyles.appTitle,
-              )
-            ],
-          )
+        padding: const EdgeInsets.all(AppConstants.kDefaultPadding),
+        child: Column(children: [Text("Notes", style: AppTextStyles.appTitle)]),
       ),
     );
   }
