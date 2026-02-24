39e9bb1412470fecb29b76d9e714194d564a5e79
Harsha <kesrabandara1998@gmail.com>
2026-02-25 01:20:55 +0530

    added rout to back button in category card

--- lib/pages/notes_by_category.dart
+++ lib/pages/notes_by_category.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:tidyup/models/note_model.dart';
 import 'package:tidyup/services/note_service.dart';
+import 'package:tidyup/utils/constants.dart';
+import 'package:tidyup/utils/router.dart';
+import 'package:tidyup/utils/text_styles.dart';
+import 'package:tidyup/widget/note_category_card.dart';
 
 class NoteByCategory extends StatefulWidget {
   final String category;
@@ -14,8 +18,8 @@ class _NoteByCategoryState extends State<NoteByCategory> {
 
   List<Note> noteList = [];
   void initState(){
-    super.initState();
     _loadCategoriesNotes();
+        super.initState();
   }
 
   // load all notes by category
@@ -25,12 +29,23 @@ class _NoteByCategoryState extends State<NoteByCategory> {
     setState(() {});
   }
   
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.category),
+           //remove the back button
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            AppRouter.router.push(
+              "/notes",
+            );
+          },
+        ),
       ),
+      
     );
   }
 }
