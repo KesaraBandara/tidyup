cc102793dc6e73a5604fa2dbb79f0e5ad338d9e5
Harsha <kesrabandara1998@gmail.com>
2026-02-25 02:19:17 +0530

    setup side box to load note and delete edit, buttos

--- lib/pages/notes_by_category.dart
+++ lib/pages/notes_by_category.dart
@@ -59,6 +59,28 @@ class _NoteByCategoryState extends State<NoteByCategory> {
                 widget.category,
                 style: AppTextStyles.appTitle,
               ),
+                            const SizedBox(
+                height: 30,
+              ),
+              GridView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: AppConstants.kDefaultPadding,
+                  mainAxisSpacing: AppConstants.kDefaultPadding,
+                  childAspectRatio: 7 / 11,
+                ),
+                itemCount: noteList.length,
+                itemBuilder: (context, index) {
+                  return NoteCategoryCard(
+                    noteTitle: noteList[index].title,
+                    noteContent: noteList[index].content,
+                    removeNote: () async {},
+                   editNote: () async {}, viewSingleNote: () {  },
+                  );
+                },
+              ),
             ],
           ),
         ),
