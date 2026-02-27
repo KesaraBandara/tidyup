53305ffc639029abeb3af8f4dec1edfe9c014ed4
Harsha <kesrabandara1998@gmail.com>
2026-02-27 17:07:33 +0530

    create  a methord to  add a new note

--- lib/services/note_service.dart
+++ lib/services/note_service.dart
@@ -55,6 +55,17 @@ class NoteService {
     return [];
   }
 
+  // Method to add a new note
+  Future<void> addNote(Note note) async {
+    try {
+      //get all notes from the box
+      final dynamic allNotes = await _myBox.get("notes");
+      allNotes.add(note);
+      await _myBox.put("notes", allNotes);
+      // ignore: empty_catches
+    } catch (e) {}
+  }
+
   //loop througn all notes and create an object where the key is the category and the value is the notes in that category
   Map<String, List<Note>> getNotesByCategoryMap(List<Note> allNotes) {
     final Map<String, List<Note>> notesByCategory = {};
