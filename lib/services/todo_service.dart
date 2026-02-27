502bb71b8d70cf1627bb0ed8ad557a9b46cbf79a
Harsha <kesrabandara1998@gmail.com>
2026-02-28 04:47:04 +0530

    create todo service class and add defald todos

---
+++ lib/services/todo_service.dart
@@ -0,0 +1,26 @@
+import 'package:hive/hive.dart';
+import 'package:tidyup/models/todo_model.dart';
+
+class TodoService {
+  List<Todo> todos = [
+    Todo(
+      title: "Read a Book",
+      date: DateTime.now(),
+      time: DateTime.now(),
+      isDone: true,
+    ),
+    Todo(
+      title: "Go for a Walk",
+      date: DateTime.now(),
+      time: DateTime.now(),
+      isDone: false,
+    ),
+    Todo(
+      title: "Complete Assignment",
+      date: DateTime.now(),
+      time: DateTime.now(),
+      isDone: false,
+    ),
+  ];
+
+}
